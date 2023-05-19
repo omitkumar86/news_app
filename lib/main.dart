612fb58f997224ac303941b5f2c8621c5e4b9c23
omitkumar86 <omitkumar86@gmail.com>
2023-05-19 21:59:41 +0600

    Update main page

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
-  //Fetch the current theme
+  /// Fetch the current theme
   void getCurrentAppTheme() async {
     themeChangeProvider.setDarkTheme =
         await themeChangeProvider.darkThemePreferences.getTheme();
